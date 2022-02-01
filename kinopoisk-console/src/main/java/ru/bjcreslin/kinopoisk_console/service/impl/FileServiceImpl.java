cb2660edac6bf7ade19dc74eaaad467244f2ae7d
bjcrelin <bjcreslin@gmail.com>
2022-02-01 23:08:52 +0700

    service has been created

--- kinopoisk-console/src/main/java/ru/bjcreslin/kinopoisk_console/service/impl/FileServiceImpl.java
+++ kinopoisk-console/src/main/java/ru/bjcreslin/kinopoisk_console/service/impl/FileServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.bjcreslin.kinopoisk_console.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.bjcreslin.kinopoisk_console.service.FileService;
 
@@ -9,13 +11,26 @@ import java.net.URL;
 @Service
 public class FileServiceImpl implements FileService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileServiceImpl.class);
+
+    private static final String HTML_DIR = "html/";
+
+    public static final String FILE_IS_NOT_FOUND = "File %s is not found!";
+
+    public static final String FILE_HAS_BEEN_LOADED = "File {} has been loaded";
+
     @Override
     public File getFileFromResources(String fileName) {
         ClassLoader classLoader = getClass().getClassLoader();
-        URL resource = classLoader.getResource("html/"+fileName);
+        URL resource = classLoader.getResource(HTML_DIR + fileName);
         if (resource == null) {
-            throw new IllegalArgumentException("file is not found!");
+            var message = String.format(FILE_IS_NOT_FOUND, fileName);
+            LOGGER.error(message);
+            throw new IllegalArgumentException(message);
         } else {
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug(FILE_HAS_BEEN_LOADED, fileName);
+            }
             return new File(resource.getFile());
         }
     }
